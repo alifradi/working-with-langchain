9895a5b01f1f3d05f18987bd9bc1791d36f8fec1
alifradi <alifradi7@gmail.com>
2025-02-11 15:00:37 -0500

    .

--- Developing LLM Applications with LangChain.py
+++ Developing LLM Applications with LangChain.py
@@ -4,8 +4,8 @@ import os
 huggingfacehub_api_token = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
 # Define the LLM
-llm = HuggingFaceEndpoint(repo_id='tiiuae/falcon-7b-instruct', huggingfacehub_api_token=huggingfacehub_api_token)
-
+# Define the LLM with the task
+llm = HuggingFaceEndpoint(repo_id='tiiuae/falcon-7b-instruct', task='text-generation', huggingfacehub_api_token=huggingfacehub_api_token)
 # Predict the words following the text in question
 question = 'Whatever you do, take care of your shoes'
 output = llm.predict(question)
